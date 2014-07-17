2cbe7a360ff944fa59e3b9bd3bf8112bd644ad3e
phoenix104104 <phoenix104104@gmail.com>
2014-07-17 22:47:29 +0800

    successful output(TODO: alignment)

--- engine.py
+++ engine.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 from flask import Flask, render_template, request, jsonify
-from StringIO import StringIO
+from parse_record import load_data_from_string, parse_game_data
+from dump_record import make_PTT_format, make_database_format
+import re
 
 app = Flask(__name__)
 
@@ -10,13 +13,18 @@ def hello():
 
 @app.route('/convert.json', methods=["POST"])
 def convert():
-    print "get content from html:"
-    data1 = request.form["text1"]
-    data2 = request.form["text2"]
-    print data1
-    print data2
+    print "get content from html..."
+    data1 = request.form["text1"].encode('utf8')
+    data2 = request.form["text2"].encode('utf8')
     
-    return jsonify({'result': "got data!"})
+    data_all = data1 + '\n' + data2
+
+    print "parse game data..."
+    raw_data = load_data_from_string(data_all)
+    game = parse_game_data(raw_data)
+    post_ptt = make_PTT_format(game, 0)
+
+    return jsonify({'result': post_ptt})
 
 if __name__ == "__main__":
     app.debug = True
